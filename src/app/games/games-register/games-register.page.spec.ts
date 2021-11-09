c1dcd41e32f258804ddbcd8594dd584e50c3860d
Willian Arcênio <willianarcenio@hotmail.com>
2021-11-09 20:49:22 -0300

    Criado página de listagem e cadastro de Games e ajustado o Menu

--- src/app/games/games-register/games-register.page.spec.ts
+++ src/app/games/games-register/games-register.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { FolderPage } from './folder.page';
 
-describe('FolderPage', () => {
-  let component: FolderPage;
-  let fixture: ComponentFixture<FolderPage>;
+import { GamesRegisterPage } from './games-register.page';
+
+describe('GamesRegisterPage', () => {
+  let component: GamesRegisterPage;
+  let fixture: ComponentFixture<GamesRegisterPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ FolderPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [ GamesRegisterPage ],
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(FolderPage);
+    fixture = TestBed.createComponent(GamesRegisterPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
