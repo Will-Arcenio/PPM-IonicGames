b0e2ef0eeb72cc4afe28de476f2a1475b5761992
Willian Arcênio <willianarcenio@hotmail.com>
2021-11-09 21:59:57 -0300

    Iniciada a listagem de games.

--- src/app/games/games-list/games-list.page.ts
+++ src/app/games/games-list/games-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game, Genero } from '../games.model';
 
 @Component({
   selector: 'app-games-list',
@@ -7,7 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesListPage implements OnInit {
 
-  constructor() { }
+  games: Game[];
+
+  constructor() {
+    this.games = [
+      {
+        nome: 'Rainbow Six:Siege',
+        genero: Genero.FPS,
+        preco: 80.00,
+        lancamento: new Date(2015,11,20),
+        foto: 'https://cdn1.epicgames.com/carnation/offer/r6s-y6-epic-std-store-landscape-2560x1440-2560x1440-ada4045f97c2-2560x1440-1a29dde6790c324f62bb12216e651944.jpeg'
+      },
+      {
+        nome: 'CS:GO', 
+        genero: Genero.FPS, 
+        lancamento: new Date(2021,10,11), 
+        preco: 29.90, 
+        foto: 'https://th.bing.com/th/id/OIP.SWYgOJ8CrbBMQuP4-jHeigHaEK?w=310&h=180&c=7&r=0&o=5&pid=1.7'
+      },
+      {
+        nome: 'FIFA 22',
+        genero: Genero.ESPORTES,
+        preco: 299.90,
+        lancamento: new Date(2022,8,17),
+        foto: 'https://tm.ibxk.com.br/2021/10/05/05164344193348.jpg'
+      },
+      {
+        nome: 'Assassins Creed Unity',
+        genero: Genero.ACAO,
+        preco: 59.90,
+        lancamento: new Date(2014,12,13),
+        foto: 'https://smartcdkeys.com/image/data/products/assassins-creed-unity-ps4/cover/assassins-creed-unity-ps4-smartcdkeys-cheap-cd-key-cover.jpg'
+     }
+    ];
+  }
 
   ngOnInit() {
   }
---
+++ src/app/games/games.model.ts
@@ -0,0 +1,19 @@
+import { GenerateOptions } from "rxjs/internal/observable/generate";
+
+export interface Game {
+    nome: string;
+    genero: Genero;
+    preco: number;
+    lancamento: Date;
+    foto: string;
+}
+
+export enum Genero{
+    RPG       = 'RPG',
+    FPS       = 'FPS',
+    ESPORTES  = 'ESPORTES',
+    ARCADE    = 'ARCADE',
+    ACAO      = 'AÇÃO',
+    TABULEIRO = 'TABULEIRO',
+    PUZZLE    = 'PUZZLE'
+}
